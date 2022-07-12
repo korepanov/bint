0f983a4e6abee2ddfe047514d0a04db7bc71a0ab
Вячеслав Корепанов <korepanov.viacheslav@yandex.ru>
2022-07-12 13:37:04 +0300

    [!] benv/func.basm

--- cmd/main.go
+++ cmd/main.go
@@ -10,8 +10,8 @@ import (
 
 func main() {
 	// эти опции можно менять для системной отладки
-	sysMode := options.UserTranslate
-	benvMode := options.ExecBenv
+	sysMode := options.ExecBasm
+	benvMode := options.InterpBenv
 
 	var filesListToExecute []string
 
