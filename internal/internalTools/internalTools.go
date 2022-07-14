f29406cea5b9f7690b4ef649f646229f737b0145
Вячеслав Корепанов <korepanov.viacheslav@yandex.ru>
2022-07-14 14:05:10 +0300

    [.]

--- internal/internalTools/internalTools.go
+++ internal/internalTools/internalTools.go
@@ -140,11 +140,11 @@ func SetConf(toTranslate int, rootSource string, rootDest string, keyDest string
 				filesListToExecute = []string{"benv/import.basm",
 					//	"benv/trace.basm"}
 					"benv/prep_func.basm",
-					"benv/prep_if.basm"}
-				//	"benv/if.basm"}
-				//"benv/long_function.basm",
-				//"benv/func.basm",
-				//"benv/print_format.basm"}
+					"benv/prep_if.basm",
+					"benv/if.basm",
+					"benv/long_function.basm",
+					"benv/func.basm",
+					"benv/print_format.basm"}
 			}
 		} else if options.Transpile == toTranslate {
 			rootSource = "benv/internal/build/if_plug.basm"
