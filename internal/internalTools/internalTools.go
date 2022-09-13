0e39f867c2c612070711568c6a2c6e30a2ed31c8
Вячеслав Корепанов <korepanov.viacheslav@yandex.ru>
2022-09-13 11:15:12 +0300

    [+] benv/internal/build/prep_dowhile [+] benv/internal/build/dowhile

--- internal/internalTools/internalTools.go
+++ internal/internalTools/internalTools.go
@@ -104,12 +104,14 @@ func SetConf(toTranslate int, rootSource string, rootDest string, keyDest string
 		toTranslate = toTranslateInternal
 
 		if options.Internal == toTranslate {
-			rootSource = "benv/dowhile.b"
-			rootDest = "benv/dowhile.basm"
+			rootSource = "benv/program.b"
+			rootDest = "benv/prog.basm"
 			if execBenv {
 				filesListToExecute = []string{"benv/internal/build/import",
 					"benv/internal/build/prep_func",
 					"benv/internal/build/prep_for",
+					"benv/internal/build/prep_dowhile",
+					"benv/internal/build/dowhile",
 					"benv/internal/build/prep_while",
 					"benv/internal/build/prep_if",
 					"benv/internal/build/while",
@@ -159,8 +161,8 @@ func SetConf(toTranslate int, rootSource string, rootDest string, keyDest string
 					"benv/print_format.basm"}
 			}
 		} else if options.Transpile == toTranslate {
-			rootSource = "benv/build/dowhile.basm"
-			rootDest = "benv/build/main.go"
+			rootSource = "benv/internal/build/prep_while.basm"
+			rootDest = "benv/internal/build/main.go"
 
 			source, err := os.Open("benv/build/pattern.p")
 			if nil != err {
@@ -188,7 +190,7 @@ func SetConf(toTranslate int, rootSource string, rootDest string, keyDest string
 		} else if options.ExecBasm == toTranslate {
 			//rootSource = "program.b"
 			//rootDest = "benv/dowhile.basm"
-			rootDest = "program.basm"
+			rootDest = "benv/prog.basm"
 			filesListToExecute = []string{rootDest}
 		} else if options.Primitive == toTranslate {
 			rootSource = "bendBenv/func.basm"
