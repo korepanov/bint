ba9dda711def85372a43b036cc4ff9f88b6e5707
Вячеслав Корепанов <korepanov.viacheslav@yandex.ru>
2022-11-12 13:55:42 +0200

    [.]

--- internal/internalTools/internalTools.go
+++ internal/internalTools/internalTools.go
@@ -196,7 +196,7 @@ func SetConf(toTranslate int, rootSource string, rootDest string, keyDest string
 		} else if options.ExecBasm == toTranslate {
 			//rootSource = "program.b"
 			//rootDest = "benv/dowhile.basm"
-			rootDest = "benv/prog.basm"
+			rootDest = "program.basm"
 			filesListToExecute = []string{rootDest}
 		} else if options.Primitive == toTranslate {
 			rootSource = "bendBenv/func.basm"
