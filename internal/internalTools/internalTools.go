f37e30d2619dc369c3ed063d229e3f125ac4cda3
Вячеслав Корепанов <korepanov.viacheslav@yandex.ru>
2022-12-09 16:11:52 +0300

    [+] internal/build/nested_call

--- internal/internalTools/internalTools.go
+++ internal/internalTools/internalTools.go
@@ -110,8 +110,8 @@ func SetConf(toTranslate int, rootSource string, rootDest string, keyDest string
 		toTranslate = toTranslateInternal
 
 		if options.Internal == toTranslate {
-			rootSource = "benv/nested_call.b"
-			rootDest = "benv/nested_call.basm"
+			rootSource = "benv/program.b"
+			rootDest = "benv/prog.basm"
 			if execBenv {
 				filesListToExecute = []string{"benv/internal/build/import",
 					"benv/internal/build/prep_func",
@@ -123,6 +123,7 @@ func SetConf(toTranslate int, rootSource string, rootDest string, keyDest string
 					"benv/internal/build/while",
 					"benv/internal/build/for",
 					"benv/internal/build/if",
+					"benv/internal/build/nested_call",
 					"benv/internal/build/long_function",
 					"benv/internal/build/recurs",
 					"benv/internal/build/func",
@@ -170,8 +171,8 @@ func SetConf(toTranslate int, rootSource string, rootDest string, keyDest string
 					"benv/print_format.basm"}
 			}
 		} else if options.Transpile == toTranslate {
-			rootSource = "benv/build/nested_call.basm"
-			rootDest = "benv/build/main.go"
+			rootSource = "benv/internal/build/long_function.basm"
+			rootDest = "benv/internal/build/main.go"
 
 			source, err := os.Open("benv/build/pattern.p")
 			if nil != err {
