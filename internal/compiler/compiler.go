fa2667e0d8e1448621a9d793151497804d9b6b88
Вячеслав Корепанов <korepanov.viacheslav@yandex.ru>
2023-10-03 17:11:15 +0300

    [+] compiler.go: float(): работа с переменными

--- internal/compiler/compiler.go
+++ internal/compiler/compiler.go
@@ -3505,9 +3505,8 @@ func compile(systemStack []interface{}, OP string, LO []interface{}, RO []interf
 			return []interface{}{true, "t" + fmt.Sprintf("%v", tNumber)}, systemStack, "int", nil
 
 		} else {
-			fmt.Println("float() with vars is not realized")
-			os.Exit(1)
-			/*_, err := progFile.Write([]byte("\nmov $lenVarName, %rsi \n mov $varName, %rdx \n mov " + lenLO +
+
+			_, err := progFile.Write([]byte("\nmov $lenVarName, %rsi \n mov $varName, %rdx \n mov " + lenLO +
 				", %rax \n mov " + fmt.Sprintf("%v", LO[0]) + ", %rdi\n call __set " +
 				"\n call __getVar \n mov (userData), %rsi \n call __len \n mov $lenBuf, %rsi \n mov $buf, %rdx \n " +
 				"mov (userData), %rdi\n call __set"))
@@ -3515,14 +3514,16 @@ func compile(systemStack []interface{}, OP string, LO []interface{}, RO []interf
 				fmt.Println(err)
 				os.Exit(1)
 			}
-			_, err = progFile.Write([]byte("\n call __userToNumber\n call __toStr \n  \n mov $lenT" + fmt.Sprintf("%v", tNumber) + ", %rsi \n mov $t" + fmt.Sprintf("%v", tNumber) +
-				", %rdx \n mov $lenBuf2, %rax \n mov $buf2, %rdi\n call __set"))
+			_, err = progFile.Write([]byte("\n call __userParseFloat\n \n movss %xmm0, (buf) \n call __floatToStr \n  \n mov $lenT" +
+				fmt.Sprintf("%v", tNumber) + ", %rsi \n mov $t" + fmt.Sprintf("%v", tNumber) +
+				", %rdx \n mov $userData, %rax \n mov $userData, %rdi\n call __set"))
+
 			if nil != err {
 				fmt.Println(err)
 				os.Exit(1)
 			}
 
-			return []interface{}{true, "t" + fmt.Sprintf("%v", tNumber)}, systemStack, "int", nil*/
+			return []interface{}{true, "t" + fmt.Sprintf("%v", tNumber)}, systemStack, "int", nil
 		}
 	} else if "bool" == OP {
 		if "\"" == string(fmt.Sprintf("%v", LO[0])[0]) {
--- program.b
+++ program.b
@@ -1,7 +1,10 @@
 void main(){
 	float a;
-	a = 7.65; 
-	a = (float(a) + 2);
+	a = 7.65;
+	float b;
+	b = 3; 
+ 
+	a = ((float(a) + float(b)) * 2);
 
 	print((str(a) + "\n"));
 	
