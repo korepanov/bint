69bbe7e08c86da8b2fa430a3a4380407375d20eb
Вячеслав Корепанов <korepanov.viacheslav@yandex.ru>
2023-11-22 16:44:06 +0300

    [!] internal: try.b: $toPanic instead of toPanic

--- internal/internalTools/internalTools.go
+++ internal/internalTools/internalTools.go
@@ -128,8 +128,8 @@ func SetConf(toTranslate int, rootSource string, rootDest string, keyDest string
 		toTranslate = toTranslateInternal
 
 		if options.Internal == toTranslate {
-			rootSource = "benv/try.b"
-			rootDest = "benv/try.basm"
+			rootSource = "benv/program.b"
+			rootDest = "benv/prog.basm"
 			if execBenv {
 				filesListToExecute = []string{"benv/internal/build/import",
 					"benv/internal/build/prep_func",
@@ -213,8 +213,8 @@ func SetConf(toTranslate int, rootSource string, rootDest string, keyDest string
 					"benv/print_format.basm"}
 			}
 		} else if options.Transpile == toTranslate {
-			rootSource = "benv/build/try.basm"
-			rootDest = "benv/build/main.go"
+			rootSource = "benv/internal/build/try.basm"
+			rootDest = "benv/internal/build/main.go"
 
 			source, err := os.Open("benv/build/pattern.p")
 			if nil != err {
